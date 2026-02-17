e6b1fbd161a8852c7bb5fcf22bc05a387a1b2694
Nikolaos Bellias <nikolaos.bellias@gmail.com>
2026-02-17 12:01:13 +0200

    Updated forgoten migrations

--- EpsilonWebApp.Data/Migrations/20260217100033_AddIndexesAndValidations.Designer.cs
+++ EpsilonWebApp.Data/Migrations/20260217100033_AddIndexesAndValidations.Designer.cs
@@ -4,6 +4,7 @@ using EpsilonWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EpsilonWebApp.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260217100033_AddIndexesAndValidations")]
+    partial class AddIndexesAndValidations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EpsilonWebApp.Data/Migrations/20260217100033_AddIndexesAndValidations.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EpsilonWebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexesAndValidations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Customers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactName",
+                table: "Customers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Customers",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Customers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_City",
+                table: "Customers",
+                column: "City");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CompanyName",
+                table: "Customers",
+                column: "CompanyName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_ContactName",
+                table: "Customers",
+                column: "ContactName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Country",
+                table: "Customers",
+                column: "Country");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Phone",
+                table: "Customers",
+                column: "Phone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_City",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_CompanyName",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_ContactName",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_Country",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_Phone",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
